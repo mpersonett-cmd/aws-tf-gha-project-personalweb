3111403f66dcb82e5290518b4baca2901f2d9998
markpersonett-tempus <mark.personett@tempus.com>
2026-06-26 13:09:59 -0400

    first TF workflow

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tf-resources-gha-bkt"
+    region = "us-east-1"
+    key = "github-actions/terraform.tfstate"
+    encrypt = true
+    dynamodb_table = "tf-resources-DDB"
+  }
+}
